a441f40599910e553d93e778bdc36ef65ac930c0
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-05-18 15:12:19 +0200

    Added Admin Area

--- BikeRental/Program.cs
+++ BikeRental/Program.cs
@@ -78,7 +78,13 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
-
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
