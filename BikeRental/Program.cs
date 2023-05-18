5eaa7f1bb00be28571c3d176ce68143cb197c2e9
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-05-18 15:17:43 +0200

    Added Roles and Admin Account, Login Fixed

--- BikeRental/Program.cs
+++ BikeRental/Program.cs
@@ -47,6 +47,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<BikeRental.Services.ApplicationDbContext>();
 builder.Services.AddControllersWithViews().AddViewOptions(options=>options.HtmlHelperOptions.ClientValidationEnabled = true);
 
@@ -78,6 +79,7 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
@@ -85,9 +87,53 @@ app.UseEndpoints(endpoints =>
       pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
     );
 });
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetService<UserManager<IdentityUser>>();
+    var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
+    var createResult = userManager.CreateAsync(
+                 new IdentityUser()
+                 {
+                     UserName = "admin@ath.eu",
+                     Email = "admin@ath.eu",
+                     LockoutEnabled = false,
+                     AccessFailedCount = 0,
+                 }, "Az123456$");
+
+    if (roleManager != null)
+    {
+        if (!roleManager.RoleExistsAsync("Administrators").Result)
+            roleManager.CreateAsync(new IdentityRole()
+            {
+                Name = "Administrator"
+            });
+        if (!roleManager.RoleExistsAsync("Operator").Result)
+            roleManager.CreateAsync(new IdentityRole()
+            {
+                Name = "Operator"
+            });
+        if (!roleManager.RoleExistsAsync("U¿ytkownik").Result)
+            roleManager.CreateAsync(new IdentityRole()
+            {
+                Name = "U¿ytkownik"
+            });
+
+        var adminUser = userManager.FindByNameAsync("admin@ath.eu").Result;
+
+        var code = userManager.GenerateEmailConfirmationTokenAsync(adminUser).Result;
+        // code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
+        var result = userManager.ConfirmEmailAsync(adminUser, code).Result;
+
+        userManager.AddToRoleAsync(adminUser, "Administrator");
+    }
+}
 
 app.Run();
