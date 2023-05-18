86d455a4d7c7ddc77267c6dc71618deb0ae93770
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-05-18 15:44:55 +0200

    Added roles controller

---
+++ BikeRental/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BikeRental.Areas.Admin.Controllers
+{
+    public class RolesController : Controller
+    {
+        public RolesController(Services.ApplicationDbContext context) 
+        {
+
+        }
+        // GET: RolesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
