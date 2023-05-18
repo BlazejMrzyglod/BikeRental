e6930ee5307129dfce5ebb771b9b8e10c2ccdf90
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-05-18 16:10:21 +0200

    Showing a list of all users and their roles

--- BikeRental/Areas/Admin/Controllers/RolesController.cs
+++ BikeRental/Areas/Admin/Controllers/RolesController.cs
@@ -1,18 +1,28 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BikeRental.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class RolesController : Controller
     {
-        public RolesController(Services.ApplicationDbContext context) 
+        private UserManager<IdentityUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        public RolesController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager) 
         {
-
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
         // GET: RolesController
         public ActionResult Index()
         {
-            return View();
+            var users = _userManager.Users;
+            var usersWithRoles = users.Select(user => new
+            {
+                UserName = user.UserName,
+                Role = _userManager.GetRolesAsync(user).Result.FirstOrDefault()
+            });
+            return View(usersWithRoles);
         }
 
     }
