2be71a778f914bad0127a675a649a03f66b70a3b
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-05-21 12:49:46 +0200

    Added RoleViewModel

---
+++ BikeRental.Models/ViewModels/RoleViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BikeRental.Models.ViewModels
+{
+	public class RoleViewModel
+	{
+		[Required]
+		[Display(Name = "Nazwa użytkownika")]
+		public string userName { get; set; }
+
+		[Required]
+		[Display(Name = "Rola")]
+		public string role { get; set; }
+    }
+}
