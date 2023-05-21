ac2197b5235607dd641b53fde657a781b7493076
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-05-21 16:06:51 +0200

    Added role changing system

--- BikeRental/Areas/Admin/Controllers/RolesController.cs
+++ BikeRental/Areas/Admin/Controllers/RolesController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Http;
 using BikeRental.Models.Models;
 using AutoMapper;
 using BikeRental.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Core.Types;
 
 namespace BikeRental.Areas.Admin.Controllers
 {
@@ -11,8 +13,8 @@ namespace BikeRental.Areas.Admin.Controllers
     public class RolesController : Controller
     {
         private UserManager<IdentityUser> _userManager;
-		private readonly IMapper _mapper;
-		public RolesController(UserManager<IdentityUser> userManager, IMapper mapper) 
+        private readonly IMapper _mapper;
+        public RolesController(UserManager<IdentityUser> userManager, IMapper mapper)
         {
             _userManager = userManager;
             _mapper = mapper;
@@ -21,13 +23,29 @@ namespace BikeRental.Areas.Admin.Controllers
         public ActionResult Index()
         {
             var users = _userManager.Users;
-			List<RoleViewModel> usersWithRoles = new List<RoleViewModel>();
-			foreach (var user in users)
-			{
-				usersWithRoles.Add(_mapper.Map<RoleViewModel>(user));
-			}
-			return View(usersWithRoles);
+            List<RoleViewModel> usersWithRoles = new List<RoleViewModel>();
+            foreach (var user in users)
+            {
+                usersWithRoles.Add(_mapper.Map<RoleViewModel>(user));
+            }
+            return View(usersWithRoles);
         }
+        //POST: RolesController
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index([Bind("userName, role")] RoleViewModel user)
+        {
+                var result = _userManager.FindByNameAsync(user.userName).Result;
+                var role = _userManager.GetRolesAsync(result).Result.FirstOrDefault();
+                if (role != user.role)
+                {
+                    _userManager.RemoveFromRoleAsync(result, role);
+                    _userManager.AddToRoleAsync(result, user.role);
+                }
+            
+            return RedirectToAction(nameof(Index));
+        }
+
 
     }
 }
