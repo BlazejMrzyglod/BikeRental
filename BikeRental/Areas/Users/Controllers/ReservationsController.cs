5537e289c6e829d93c77f5be9e755dba8329ca51
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-05-24 15:17:43 +0200

    Create ReservationsController.cs

---
+++ BikeRental/Areas/Users/Controllers/ReservationsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BikeRental.Areas.Users.Controllers
+{
+	public class ReservationsController : Controller
+	{
+		// GET: ReservationsController
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		// POST: ReservationsController/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Index(IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+	}
+}
