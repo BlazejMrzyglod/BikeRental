4efc1e023d46a5fb5a0d1cc3c7cb1f64c7e43038
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-05-31 13:13:36 +0200

    Update ReservationsController.cs

--- BikeRental/Areas/Users/Controllers/ReservationsController.cs
+++ BikeRental/Areas/Users/Controllers/ReservationsController.cs
@@ -1,30 +1,42 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using BikeRental.Services.Repository.EntityFramework;
+using BikeRental.Services.Repository;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using BikeRental.Models.ViewModels;
 
 namespace BikeRental.Areas.Users.Controllers
 {
 	public class ReservationsController : Controller
 	{
-		// GET: ReservationsController
-		public ActionResult Index()
+        private readonly IRepositoryService<Models.Models.Reservation> _repository;
+        private readonly IMapper _mapper;
+
+        public ReservationsController(Services.ApplicationDbContext context, IMapper mapper)
+        {
+            _repository = new RepositoryService<Models.Models.Reservation>(context);
+            _mapper = mapper;
+        }
+        // GET: ReservationsController
+        public ActionResult Index()
 		{
 			return View();
 		}
 
-		// POST: ReservationsController/Create
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Index(IFormCollection collection)
-		{
-			try
-			{
-				return RedirectToAction(nameof(Index));
-			}
-			catch
-			{
-				return View();
-			}
-		}
+        // POST: ReservationsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index([Bind("ReservationDate,StartDate,EndDate,IsActive,Vehicles")] ReservationViewModel reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                reservation.Id = Guid.NewGuid();
+                _repository.Add(_mapper.Map<Models.Models.Reservation>(reservation));
+                _repository.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(reservation);
+        }
 
-	}
+    }
 }
