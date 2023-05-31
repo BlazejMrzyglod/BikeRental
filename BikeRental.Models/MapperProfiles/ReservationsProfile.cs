868b08a108a82fd2ada5d5f30f7a72efb3b9faa6
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-05-31 15:23:36 +0200

    Create ReservationsProfile.cs

---
+++ BikeRental.Models/MapperProfiles/ReservationsProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using BikeRental.Models
+using BikeRental.Models.Models;
+using BikeRental.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRental.Models.MapperProfiles
+{
+    public class ReservationsProfile : Profile
+    {
+        public ReservationsProfile() 
+        {
+            CreateMap<Reservation, ReservationViewModel>();
+            CreateMap<ReservationViewModel, Reservation>().ForMember(dest => dest.User, opt => opt.MapFrom<ReservationResolver>());
+        }
+    }
+}
