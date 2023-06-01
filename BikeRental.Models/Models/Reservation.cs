cca65005a52424b96c02626d09d765d857009928
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-06-01 11:02:25 +0200

    Changed IsActive to Status in ReservationModel

--- BikeRental.Models/Models/Reservation.cs
+++ BikeRental.Models/Models/Reservation.cs
@@ -6,14 +6,20 @@ using System.Threading.Tasks;
 
 namespace BikeRental.Models.Models
 {
-    public class Reservation : IEntity<Guid>
+	public enum Status
+	{
+		Realizacja,
+		Wypożyczone,
+		Zwrócone
+	}
+	public class Reservation : IEntity<Guid>
     {
         public Guid Id { get; set; }
         public DateTime ReservationDate { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public bool IsActive { get; set; }
-        public string? User { get; set; }
+		public Status Status { get; set; }
+		public string? User { get; set; }
         public Guid VehicleId { get; set; }
         public Vehicle? Vehicle { get; set; }
     }
