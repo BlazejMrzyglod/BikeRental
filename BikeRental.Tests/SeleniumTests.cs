64a4272b202d5dcc23dc3c4857f852cc38446364
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-06-13 20:41:42 +0200

    Finished main reservation test

--- BikeRental.Tests/SeleniumTests.cs
+++ BikeRental.Tests/SeleniumTests.cs
@@ -13,15 +13,11 @@ namespace BikeRental.Tests
             ChromeDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://localhost:7296");
             driver.FindElement(By.PartialLinkText("Rowery")).Click();
-            Assert.Equal("Rowery - ATH Bike Rental System", driver.Title);
             driver.FindElement(By.PartialLinkText("Szczegó³y")).Click();
-            Assert.Equal("Szczegó³y - ATH Bike Rental System", driver.Title);
             driver.FindElement(By.PartialLinkText("Zarezerwuj")).Click();
-            Assert.Equal("Log in - ATH Bike Rental System", driver.Title);
             driver.FindElement(By.Id("Input_Email")).SendKeys("admin@ath.eu");
             driver.FindElement(By.Id("Input_Password")).SendKeys("Az123456$");
             driver.FindElement(By.Id("login-submit")).Click();
-            Assert.Equal("Stwórz rezerwacje - ATH Bike Rental System", driver.Title);
             var startDate = driver.FindElement(By.Id("StartDate"));
             startDate.SendKeys("25102023");
             startDate.SendKeys(Keys.Tab);
@@ -31,19 +27,23 @@ namespace BikeRental.Tests
             endDate.SendKeys(Keys.Tab);
             endDate.SendKeys("1000AM");
             driver.FindElement(By.Id("create")).Click();
-            Assert.Equal("Rezerwacja udana - ATH Bike Rental System", driver.Title);
             driver.FindElement(By.PartialLinkText("Powróæ")).Click();
-            Assert.Equal("Strona g³ówna - ATH Bike Rental System", driver.Title);
             driver.FindElement(By.PartialLinkText("Rowery")).Click();
-            Assert.Equal("Rowery - ATH Bike Rental System", driver.Title);
             driver.FindElement(By.PartialLinkText("Szczegó³y")).Click();
-            Assert.Equal("Szczegó³y - ATH Bike Rental System", driver.Title);
             Assert.Empty(driver.FindElements(By.PartialLinkText("Zarezerwuj")));
             driver.Navigate().GoToUrl("https://localhost:7296/Admin/Reservations");
+            driver.FindElement(By.PartialLinkText("Zmieñ")).Click();
+			driver.FindElement(By.Name("rent")).Click();
+			driver.FindElement(By.PartialLinkText("Zmieñ")).Click();
+			driver.FindElement(By.Name("return")).Click();
+			driver.FindElement(By.PartialLinkText("Zmieñ")).Click();
+            Assert.Empty(driver.FindElements(By.Id("change")));
+            driver.FindElement(By.PartialLinkText("Wróæ")).Click();
             Assert.Equal("Index - Admin ATH Bike Rental System", driver.Title);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            driver.FindElement(By.PartialLinkText("Zmieñ status")).Click();
-            //driver.Quit();
+            driver.Navigate().GoToUrl("https://localhost:7296/Vehicles");
+            driver.FindElement(By.PartialLinkText("Szczegó³y")).Click();
+            driver.FindElement(By.PartialLinkText("Zarezerwuj")).Click();
+            driver.Quit();
         }
     }
 }
