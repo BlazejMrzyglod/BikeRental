22f76cea18d37f650bb539ea4f690cb43ba744a6
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-06-13 21:44:38 +0200

    Added date validator test

--- BikeRental.Tests/SeleniumTests.cs
+++ BikeRental.Tests/SeleniumTests.cs
@@ -8,9 +8,10 @@ namespace BikeRental.Tests
     public class SeleniumTests
     {
         [Fact]
-        public void ReservationsTest()
+        public void ReservationsMainTest()
         {
             ChromeDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl("https://localhost:7296");
             driver.FindElement(By.PartialLinkText("Rowery")).Click();
             driver.FindElement(By.PartialLinkText("Szczegó³y")).Click();
@@ -42,7 +43,40 @@ namespace BikeRental.Tests
             Assert.Equal("Index - Admin ATH Bike Rental System", driver.Title);
             driver.Navigate().GoToUrl("https://localhost:7296/Vehicles");
             driver.FindElement(By.PartialLinkText("Szczegó³y")).Click();
+            driver.FindElement(By.PartialLinkText("Zarezerwuj"));
+            driver.Quit();
+        }
+
+        [Fact]
+        public void ReservationDateValidatorTest()
+        {
+            ChromeDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://localhost:7296");
+            driver.FindElement(By.PartialLinkText("Rowery")).Click();
+            driver.FindElement(By.PartialLinkText("Szczegó³y")).Click();
             driver.FindElement(By.PartialLinkText("Zarezerwuj")).Click();
+            driver.FindElement(By.Id("Input_Email")).SendKeys("admin@ath.eu");
+            driver.FindElement(By.Id("Input_Password")).SendKeys("Az123456$");
+            driver.FindElement(By.Id("login-submit")).Click();
+            var startDate = driver.FindElement(By.Id("StartDate"));
+            startDate.SendKeys("25102022");
+            startDate.SendKeys(Keys.Tab);
+            startDate.SendKeys("1000AM");
+            var endDate = driver.FindElement(By.Id("EndDate"));
+            endDate.SendKeys("30102023");
+            endDate.SendKeys(Keys.Tab);
+            endDate.SendKeys("1000AM");
+            Assert.Equal("Stwórz rezerwacje - ATH Bike Rental System", driver.Title);
+            startDate = driver.FindElement(By.Id("StartDate"));
+            startDate.SendKeys("25102023");
+            startDate.SendKeys(Keys.Tab);
+            startDate.SendKeys("1000AM");
+            endDate = driver.FindElement(By.Id("EndDate"));
+            endDate.SendKeys("24102023");
+            endDate.SendKeys(Keys.Tab);
+            endDate.SendKeys("1000AM");
+            Assert.Equal("Stwórz rezerwacje - ATH Bike Rental System", driver.Title);
             driver.Quit();
         }
     }
